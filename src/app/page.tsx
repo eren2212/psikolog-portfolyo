324a1a1355a08bbc2b95ce629b8fa773d27e4760
eren iridere <eiridere@gmail.com>
2025-08-13 21:57:15 +0300

    Update main page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import ProfessionalField from "./components/Content/ProfessionalField";
+import HowFeeling from "./components/Content/HowFeeling";
+import WhereStart from "./components/Content/WhereStart";
+import Comment from "./components/Content/Comment";
+import Footer from "./components/Footer";
+import Container from "./components/Container";
 
 export default function Home() {
   return (
     <div className="min-h-screen ">
       <Navbar />
+      <Container>
+        <Hero />
+        <ProfessionalField />
+        <WhereStart />
+        <HowFeeling />
+        <Comment />
+      </Container>
+      <Footer />
     </div>
   );
 }
