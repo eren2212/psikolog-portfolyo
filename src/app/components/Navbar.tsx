815afafecaae9a3fc85f5850b148fd7f411a4a7d
eren iridere <eiridere@gmail.com>
2025-08-13 21:56:54 +0300

    Update Navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Button from "./Button";
 import Image from "next/image";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { motion } from "framer-motion";
+import { FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="  sticky top-0 z-50  py-5">
+    <nav className="  sticky top-0 z-50  py-5  backdrop-blur-md w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo Section */}
@@ -52,54 +57,51 @@ const Navbar = () => {
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-950 transition-colors duration-200"
               aria-controls="mobile-menu"
               aria-expanded="false"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
             >
               <span className="sr-only">Ana menüyü aç</span>
               {/* Hamburger icon */}
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
+              {isOpen ? <FaTimes /> : <GiHamburgerMenu />}
             </button>
           </div>
         </div>
 
         {/* Mobile menu */}
-        <div className="md:hidden" id="mobile-menu">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 border-t border-gray-100 mt-2">
-            <a
-              href="#"
-              className="text-gray-700 hover:text-gray-900 block px-3 py-2 text-base font-medium transition-colors duration-200 ease-in-out"
-            >
-              Anasayfa
-            </a>
-            <a
-              href="#"
-              className="text-gray-700 hover:text-gray-900 block px-3 py-2 text-base font-medium transition-colors duration-200 ease-in-out"
-            >
-              Hakkımda
-            </a>
-            <a
-              href="#"
-              className="text-gray-700 hover:text-gray-900 block px-3 py-2 text-base font-medium transition-colors duration-200 ease-in-out"
-            >
-              İletişim
-            </a>
-            <Button fullWidth>
-              <a href="/randevu-al">Randevu Al</a>
-            </Button>
-          </div>
-        </div>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="md:hidden"
+            id="mobile-menu"
+          >
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3  mt-2">
+              <a
+                href="#"
+                className="text-gray-700 hover:text-gray-900 block px-3 py-2 text-base font-medium transition-colors duration-200 ease-in-out"
+              >
+                Anasayfa
+              </a>
+              <a
+                href="#"
+                className="text-gray-700 hover:text-gray-900 block px-3 py-2 text-base font-medium transition-colors duration-200 ease-in-out"
+              >
+                Hakkımda
+              </a>
+              <a
+                href="#"
+                className="text-gray-700 hover:text-gray-900 block px-3 py-2 text-base font-medium transition-colors duration-200 ease-in-out"
+              >
+                İletişim
+              </a>
+              <Button fullWidth>
+                <a href="/randevu-al">Randevu Al</a>
+              </Button>
+            </div>
+          </motion.div>
+        )}
       </div>
     </nav>
   );
